fc6599c1b29f9039c349d645bfef7e9753e4bcfa
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-08-26 16:46:30 +0100

    menu list updated

--- version_1/NavigationUI/MenuListUI/menu_list_ui_controller.js
+++ version_1/NavigationUI/MenuListUI/menu_list_ui_controller.js
@@ -1,10 +1,25 @@
 
-import BaseController from "../../Base/base_controller";
-import MenuListUIConfig from "./menu_list_ui_config";
+import BaseController       from "../../Base/base_controller";
+import MenuListUIConfig     from "./menu_list_ui_config";
+
+import NavLinkUI            from "../NavLinkUI/nav_link_ui.vue";
 
 class MenuListUIController extends BaseController { 
-    constructor() {
-        super("menu_list_ui_controller", new MenuListUIConfig());
+    constructor() { super("menu_list_ui", MenuListUIConfig); }
+
+    // Method to get ui components
+    getUIComponents() { return { NavLinkUI }; }
+
+    // Method to get ui props
+    getUIProps() { 
+        return {
+            // Class styles
+            wrapper_class_style: { type: String, default: "", required: false },
+
+            second_level_wrapper_class_style: { type: String, default: "pl-4 border-l border-gray-200", required: false },
+
+            menus: { type: Array, required: true },
+        }
     }
 }
 
