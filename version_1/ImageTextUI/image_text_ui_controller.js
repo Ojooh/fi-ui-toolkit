93bc9c2b6f3cb53597b1d9841d336edd923fab2d
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-08-26 16:22:56 +0100

    update image text ui

--- version_1/ImageTextUI/image_text_ui_controller.js
+++ version_1/ImageTextUI/image_text_ui_controller.js
@@ -1,12 +1,27 @@
 
 
 
-import BaseController               from "../Base/base_controller";
+import BaseController            from "../Base/base_controller";
 import ImageTextUIConfig         from "./image_text_ui_config";
 
 class ImageTextUIController extends BaseController { 
-    constructor() {
-        super("image_text_ui_controller", new ImageTextUIConfig());
+    constructor() { super("image_text_ui", ImageTextUIConfig); }
+
+    // Method to get ui props
+    getUIProps() { 
+        return {
+            text: { type: String, required: false, default: null },
+            
+            text_class: { type: String, default: "text-black" },
+
+            image_src: { type: String, required: false, default: null },
+
+            img_alt: { type: String, required: false, default: null },
+
+            img_class_style: { type: String, required: false, default: "" },
+
+            wrapper_class_style: { type: String, required: false, default: "" },
+        }
     }
 
 }
