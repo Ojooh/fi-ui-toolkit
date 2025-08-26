eb4ceb4959329c93a60d116873c9a642ea22e06f
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-08-26 13:28:49 +0100

    updated footer copy right

--- version_1/FooterCopyRightUI/footer_copy_right_ui_controller.js
+++ version_1/FooterCopyRightUI/footer_copy_right_ui_controller.js
@@ -3,8 +3,21 @@ import BaseController               from "../Base/base_controller";
 import FooterCopyRightUIConfig      from "./footer_copy_right_ui_config";
 
 class FooterCopyRightUIController extends BaseController { 
-    constructor() {
-        super("footer_copy_right_ui_controller", new FooterCopyRightUIConfig());
+    constructor() { super("footer_copy_right_ui", FooterCopyRightUIConfig); }
+
+    // Method to get ui props
+    getUIProps() { 
+        const current_year = new Date().getFullYear().toString();
+
+        return {
+            class_style_text: { type: String, default: null, required: false },
+
+            year: { type: String, default: current_year, required: false },
+
+            powered_by_text: { type: String, required: true },
+
+            author_text: { type: String, required: true },
+        }
     }
 
 }
