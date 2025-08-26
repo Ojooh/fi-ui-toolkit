a5c92b0d4543f72cd02a2d02d1a8b74922c79e27
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-08-26 16:35:32 +0100

    updated breadcrumb ui

--- version_1/NavigationUI/BreadcrumbUI/breadcrumb_ui_controller.js
+++ version_1/NavigationUI/BreadcrumbUI/breadcrumb_ui_controller.js
@@ -1,6 +1,6 @@
 
 import BaseController from "../../Base/base_controller";
-import BreadcrumbUIConfig from "./breadcrumn_ui_config";
+import BreadcrumbUIConfig from "./breadcrumb_ui_config";
 
 class BreadcrumbUIController extends BaseController { 
     constructor() { super("breadcrumb_ui", BreadcrumbUIConfig); }
