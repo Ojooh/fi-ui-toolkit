a5b160d2e82fa291e9e671e03c23291bcc1b851f
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-08-26 16:57:15 +0100

    updated top bar ui

--- version_1/NavigationUI/TopBarUI/top_bar_ui_config.js
+++ version_1/NavigationUI/TopBarUI/top_bar_ui_config.js
@@ -1,43 +1,8 @@
 
-import BaseConfig           from "../../Base/base_config";
+import BaseConfig   from "../../Base/base_config";
 
 class TopBarUIConfig extends BaseConfig { 
-    constructor() { super("top_bar_ui_config"); }
-
-    // Method to set vue instance
-    setVueInstance(vue_instance) {
-        this.vue_instance       = vue_instance;
-        this.content_manager    = this.vue_instance?.proxy?.$content_manager || {};
-    }
-
-    // Method to get ui props
-    getUIProps() { 
-        const config_default = { bg_color: "#052146", has_shadow: true, height: 62 };
-        
-        return {
-            top_bar_class_style: { type: String, default: "", required: false },
-
-            config: { type: Object, default: config_default, required: false },
-
-            section_1_component: {type: Object, default: null, required: false },
-
-            section_1_props: { type: Object, default: null, required: false },
-
-            section_2_component: {type: Object, default: null, required: false },
-
-            section_2_props: { type: Object, default: null, required: false },
-
-            section_3_component: {type: Object, default: null, required: false },
-
-            section_3_props: { type: Object, default: null, required: false }
-        }
-    }
-
-    // Method to get ui state data
-    getUIStateData() { 
-        const util = this.util;
-        return { util }; 
-    }
+    constructor(name) { super(name); }
 
 }
 
