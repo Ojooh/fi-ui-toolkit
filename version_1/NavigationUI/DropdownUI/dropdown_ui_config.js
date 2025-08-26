4432aef37dac9064b3437417db5d66ee335f58b0
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-08-26 16:44:04 +0100

    updated dropdown ui

--- version_1/NavigationUI/DropdownUI/dropdown_ui_config.js
+++ version_1/NavigationUI/DropdownUI/dropdown_ui_config.js
@@ -1,48 +1,16 @@
 
 import BaseConfig           from "../../Base/base_config";
 import DropdownUIUtil       from "./dropdown_ui_util";
-import NavLinkUI            from "../NavLinkUI/nav_link_ui.vue";
+
 
 class  DropdownUIConfig extends BaseConfig { 
-    constructor() { super("dropdown_ui_config"); }
+    constructor(name) { super(name); }
 
     // Method to set vue instance
     setVueInstance(vue_instance) {
         this.vue_instance       = vue_instance;
         this.content_manager    = this.vue_instance?.proxy?.$content_manager || {};
         this.util               = new DropdownUIUtil(vue_instance, this.content_manager);
-    }
-
-    // Method to get ui components
-    getUIComponents() { return { NavLinkUI }; }
-
-    // Method to get ui props
-    getUIProps() { 
-        return {
-            parent_class_style: { type: String, default: "", required: false },
-
-            btn_class_style: { type: String, default: "", required: false },
-
-            menu_parent_class_style: { type: String, default: "", required: false },
-
-            menu_class_style: { type: String, default: "", required: false },
-
-            menu_list_class_style: { type: String, default: "", required: false },
-
-            btn_id: { type: String, required: true },
-
-            menu_id: { type: String, required: true },
-
-            btn_content: { type: String, required: true },
-
-            menu_list: { type: Array, required: true, default: [] },
-        }
-    }
-    
-    // Method to get ui state data
-    getUIStateData() { 
-        const util = this.util;
-        return { util }; 
     }
 
 }
