fa8bd68510a9e71ba151d2aeb0c89f78572a537f
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-08-26 16:52:53 +0100

    updated nav link ui

--- version_1/NavigationUI/NavLinkUI/nav_link_ui_config.js
+++ version_1/NavigationUI/NavLinkUI/nav_link_ui_config.js
@@ -3,7 +3,7 @@ import BaseConfig           from "../../Base/base_config";
 import NavLinkUIUtil        from "./nav_link_ui_util";
 
 class  NavLinkUIConfig extends BaseConfig { 
-    constructor() { super("nav_link_ui_config"); }
+    constructor(name) { super(name); }
 
     // Method to check if link is router link
     isRouterLink = (instance_variables) => {
@@ -34,61 +34,6 @@ class  NavLinkUIConfig extends BaseConfig {
         this.content_manager    = this.vue_instance?.proxy?.$content_manager || {};
         this.util               = new NavLinkUIUtil(vue_instance, this.content_manager);
     }
-
-    // Method to get ui components
-    getUIComponents() { return {}; }
-
-    // Method to get ui props
-    getUIProps() { 
-        return {
-            // Class styles
-            wrapper_class_style: { type: String, default: "", required: false },
-
-            active_menu_class_style: { type: String, default: "", required: false },
-
-            icon_parent_class_style: { type: String, default: "", required: false },
-
-            icon_img_class_style: { type: String, default: "", required: false },
-
-            svg_icon_class_style: { type: String, default: "", required: false },
-
-            content_class_style: { type: String, default: "", required: false },
-
-            menu_id: { type: String, required: true },
-    
-            link: { type: String, default: null, required: false },
-
-            icon_img_link: { type: String, default: null, required: false },
-
-            icon_img_alt_text: { type: String, default: "", required: false },
-
-            svg_icon: { type: String, default: null, required: false },
-
-            html_content: { type: String, default: "", required: false },
-
-            on_click: { type: Function, default: null, required: false },
-
-            is_active: { type: Function, default: () => { return false }, required: false },
-        }
-    }
-
-    // Method to get ui computed data
-    getUIComputedData() { 
-        return {
-            is_router_link: this?.isRouterLink,
-
-            is_anchor: this?.isAnchor,
-
-            component_type: this?.componentType
-        };
-    }
-
-    // Method to get ui state data
-    getUIStateData() { 
-        const util = this.util;
-        return { util }; 
-    }
-
 }
 
 export default NavLinkUIConfig;
--- version_1/NavigationUI/NavLinkUI/nav_link_ui_controller.js
+++ version_1/NavigationUI/NavLinkUI/nav_link_ui_controller.js
@@ -1,13 +1,57 @@
 
 
 
-import BaseController from "../../Base/base_controller";
-import NavLinkUIConfig from "./nav_link_ui_config";
+import BaseController   from "../../Base/base_controller";
+import NavLinkUIConfig  from "./nav_link_ui_config";
 
 class NavLinkUIController extends BaseController { 
-    constructor() {
-        super("nav_link_ui_controller", new NavLinkUIConfig());
+    constructor() { super("nav_link_ui", NavLinkUIConfig); }
+
+    // Method to get ui props
+    getUIProps() { 
+        return {
+            // Class styles
+            wrapper_class_style: { type: String, default: "", required: false },
+
+            active_menu_class_style: { type: String, default: "", required: false },
+
+            icon_parent_class_style: { type: String, default: "", required: false },
+
+            icon_img_class_style: { type: String, default: "", required: false },
+
+            svg_icon_class_style: { type: String, default: "", required: false },
+
+            content_class_style: { type: String, default: "", required: false },
+
+            menu_id: { type: String, required: true },
+    
+            link: { type: String, default: null, required: false },
+
+            icon_img_link: { type: String, default: null, required: false },
+
+            icon_img_alt_text: { type: String, default: "", required: false },
+
+            svg_icon: { type: String, default: null, required: false },
+
+            html_content: { type: String, default: "", required: false },
+
+            on_click: { type: Function, default: null, required: false },
+
+            is_active: { type: Function, default: () => { return false }, required: false },
+        }
     }
+
+    // Method to get ui computed data
+    getUIComputedData() { 
+        return {
+            is_router_link: this?.isRouterLink,
+
+            is_anchor: this?.isAnchor,
+
+            component_type: this?.componentType
+        };
+    }
+
 }
 
 export default new NavLinkUIController().getUIComponentDefinition();
