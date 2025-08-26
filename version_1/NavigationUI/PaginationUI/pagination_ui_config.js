fde8a4711ee00745055bdc3504effe6eac06c40d
David Matt-Ojo <ojooh@Davids-MacBook-Air.local>
2025-08-26 16:55:19 +0100

    updated pagination ui

--- version_1/NavigationUI/PaginationUI/pagination_ui_config.js
+++ version_1/NavigationUI/PaginationUI/pagination_ui_config.js
@@ -4,7 +4,7 @@ import PaginationUIUtil     from "./pagination_ui_util";
 
 
 class PaginationUIConfig extends BaseConfig { 
-    constructor() { super("pagination_ui_config"); }
+    constructor(name) { super(name); }
 
     // Method to set vue instance
     setVueInstance(vue_instance) {
@@ -13,58 +13,11 @@ class PaginationUIConfig extends BaseConfig {
         this.util               = new PaginationUIUtil(vue_instance, this.content_manager);
     }
 
-    // Method to get ui props
-    getUIProps() { 
-        return {               
-            wrapper_class_style: { type: String, default: "", required: false },
-
-            prev_button_class_style: { type: String, default: "", required: false },
-
-            next_button_class_style: { type: String, default: "", required: false },
-
-            disabled_class_style: { type: String, default: "", required: false }, 
-
-            select_class_style: { type: String, default: "", required: false },
-
-            prev_btn_content: { type: String, default: "Prev", required: false }, 
-
-            next_btn_content: { type: String, default: "Next", required: false },
-
-            renderPageContent: { type: Function, default: (page) => `Page ${page}`, required: false },
-
-            total_pages: { type: Number, required: true },
-
-            current_page: { type: Number, required: true },
-
-            handleOnPrevBtnClicked: { type: Function, default: () => {}, required: false },
-
-            handleOnNextBtnClicked: { type: Function, default: () => {}, required: false },
-
-            handleOnNewPageSelected: { type: Function, default: () => {}, required: false },
-        }
-    }
-
-
-    // Method to get ui watchers
-    getUIWatchers() { 
-        return {
-            current_page: { 
-                immediate: true, 
-                deep: true, 
-                handler: (new_value, old_value) => { 
-                    let selected_page = this.vue_instance.proxy.$data;
-                    selected_page = new_value;
-                } 
-            },
-        }; 
-    }
-
     // Method to get ui state data
     getUIStateData() { 
-        const util          = this.util;
-        const selected_page = this.vue_instance?.props?.current_page;
+        const selected_page = this.vue_instance?.proxy.$props?.current_page;
 
-        return { util, selected_page }
+        return { selected_page }
     }
 }
 
